1b449d58d4e402d46373ba7a8493eefff0c24133
Mullah Abdul Khadeer <134354820+Mullah-Abdul-Khadeer@users.noreply.github.com>
2024-01-25 19:58:40 +0300

    Update hand detection python code.py

--- hand detection python code.py
+++ hand detection python code.py
@@ -2,17 +2,17 @@ from cvzone.SerialModule import SerialObject
 from cvzone.HandTrackingModule import HandDetector
 import cv2
 import mediapipe as mp
-
+import openpyxl
+workbook = openpyxl.Workbook()
+sheet = workbook.active
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8, maxHands=2)
-mySerial = SerialObject("COM7", 9600)
+mySerial = SerialObject("COM7", 9600) # here the COM7 is the USD port of your device the name should be changed accordingly to your device port name  and the "9600" is a bit rate of arduino uno it also has to be changed according to the micro controller device  used 
 while True:
 
     success, img = cap.read()
-    hands, img = detector.findHands(img)  # with draw
-    # hands = detector.findHands(img, draw=False) # without draw
+    hands, img = detector.findHands(img)  
     if hands:
-        # Hand 1
         hand1 = hands[0]
         lmList1 = hand1["lmList"]
         bbox1 = hand1["bbox"]
@@ -26,6 +26,8 @@ while True:
     cv2.imshow("Image", img)
     if cv2.waitKey(1) == ord('q'):
         break
-
+for row in data:
+    sheet.append(row)
+workbook.save("model_data.xlsx")    
 cap.release()
 cv2.destroyAllWindows()
